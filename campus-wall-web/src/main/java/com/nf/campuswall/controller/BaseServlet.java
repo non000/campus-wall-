2984a72804cca8a68e138b150a45550da78735cc
CC-mm <2569379986@qq.com>
2021-06-11 11:30:36 +0800

    '更新'

---
+++ campus-wall-web/src/main/java/com/nf/campuswall/controller/BaseServlet.java
@@ -0,0 +1,13 @@
+package com.nf.campuswall.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "BaseServlet")
+public class BaseServlet extends HttpServlet {
+
+}
